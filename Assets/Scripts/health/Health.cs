6d97bdf1b5ccac5ec5176f9d328024d52679150f
charliep101 <523921@chrome.escg.ac.uk>
2021-11-13 17:57:01 +0000

    made respawn system

--- Assets/Scripts/health/Health.cs
+++ Assets/Scripts/health/Health.cs
@@ -6,16 +6,28 @@ public class Health : MonoBehaviour
 {
    [SerializeField] private float startingHealth;
     public float currentHealth;
+    private Vector3 respawnPoint;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        respawnPoint = transform.position;
     }
     public void TakeDamage(float _damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
-     
-
+       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (currentHealth < 0.01)
+        {
+            transform.position = respawnPoint;
+        }
+        else if (collision.tag == "Checkpoint")
+        {
+            respawnPoint = transform.position;
+        }
     }
 }
